1da073bb9eddf6a590d13b51a28b5a3135540b41
Robert <robert.covu@gmail.com>
2021-10-19 22:13:18 -0400

    WebAccess

--- WebAccess/ViewController.swift
+++ WebAccess/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func nextViewBTN(_ sender: UIButton) {
+        let nextView = storyboard?.instantiateViewController(withIdentifier: "WebsiteView") as! WebsiteAccess
+        
+        present(nextView, animated: true, completion: nil)
+        
+    }
+    
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    
+    
 }
 
